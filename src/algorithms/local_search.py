d73c55f787dbfbb58435b9df78a470e3c801a842
Marek Szydlowski <marek.szydlow@gmail.com>
2022-11-17 01:09:40 +0100

    Adjused local search algo for new deltas

--- src/algorithms/local_search.py
+++ src/algorithms/local_search.py
@@ -1,7 +1,15 @@
 from itertools import combinations, product
-from random import choice, shuffle
+from random import shuffle
 
-from src.model import DeltaNodes, DistanceMatrix, Node, Run
+from src.model import (
+    ADelta,
+    DeltaInterNodes,
+    DeltaIntraEdges,
+    DeltaIntraNodes,
+    DistanceMatrix,
+    Node,
+    Run,
+)
 
 
 def local_search(
@@ -17,10 +25,10 @@ def local_search(
     current_run = Run(0, initial_solution, distance_matrix)
 
     while True:
-        neighbourhood_inter = _inter(current_run.nodes, nodes_set)
-        neighbourhood_intra = _intra(current_run.nodes, intra_type)
-        neighbourhood = neighbourhood_inter + neighbourhood_intra
-
+        neighbourhood_inter = _inter(current_run.nodes, nodes_set, distance_matrix)
+        neighbourhood_intra = _intra(current_run.nodes, intra_type, distance_matrix)
+        neighbourhood: list[ADelta] = neighbourhood_inter + neighbourhood_intra
+        
         if search_type == "steepest":
             if (best_delta := min(neighbourhood)) < 0:
                 current_run = Run.from_delta(current_run, best_delta)
@@ -44,33 +52,38 @@ def local_search(
     return current_run.nodes
 
 
-def _inter(original_sequence: list[Node], nodes: set[Node]) -> list[DeltaNodes]:
+def _inter(
+    original_sequence: list[Node], nodes: set[Node], distance_matrix: DistanceMatrix
+) -> list[DeltaInterNodes]:
     og_seq = set(original_sequence)
     changes = product(og_seq, nodes - og_seq)
 
-    neighbourhood: list[DeltaNodes] = []
+    neighbourhood: list[DeltaInterNodes] = []
     for change in changes:
         inter_node, outer_node = change
-        neighbourhood.append(DeltaNodes(inter_node, outer_node, is_outer=True))
+        neighbourhood.append(DeltaInterNodes(inter_node, outer_node, distance_matrix))
 
     return neighbourhood
 
 
-def _intra(original_sequence: list[Node], intra_type: str):
+def _intra(
+    original_sequence: list[Node], intra_type: str, distance_matrix: DistanceMatrix
+) -> list[DeltaIntraNodes] | list[DeltaIntraEdges]:
     if intra_type == "nodes":
-        return _nodes_intra(original_sequence)
+        return _nodes_intra(original_sequence, distance_matrix)
     elif intra_type == "edges":
-        return _edges_intra(original_sequence)
+        return _edges_intra(original_sequence, distance_matrix)
     else:
         raise ValueError("Invalid intra_type")
 
 
-def _edges_intra(original_sequence: list[Node]) -> list[list[Node]]:
-    all_edges = [(i, i + 1) for i in range(len(original_sequence) - 1)]
-    all_edges.append((len(original_sequence) - 1, 0))
+def _edges_intra(
+    original_sequence: list[Node], distance_matrix: DistanceMatrix
+) -> list[DeltaIntraEdges]:
+    # all_edges = [(node, original_sequence[i + 1]) for i, node in all_edges]
+    all_edges = [(node, original_sequence[i + 1]) for i, node in enumerate(original_sequence[:-1])]
+    all_edges.append((original_sequence[-1], original_sequence[0]))
     edge_swaps = list(combinations(all_edges, 2))
-
-    # n*(n - 3)/2
 
     neighbourhood = []
 
@@ -80,28 +93,22 @@ def _edges_intra(original_sequence: list[Node]) -> list[list[Node]]:
         if left[1] == right[0] or left[0] == right[1]:
             continue
 
-        shift = left[0]
-        right_start_shifted = abs(right[0] - shift)
+        outerA, outerB = left[0], right[1]
 
-        shifted = original_sequence[shift:] + original_sequence[:shift]
-        neighbourhood.append(
-            shifted[:1] + shifted[right_start_shifted:0:-1] + shifted[right_start_shifted + 1 :]
-        )
+        neighbourhood.append(DeltaIntraEdges(outerA, outerB, distance_matrix))
 
+    # Size: n*(n - 3)/2
     return neighbourhood
 
 
-def _nodes_intra(original_sequence: list[Node]) -> list[list[Node]]:
-    # n*(n - 1)/2
+def _nodes_intra(
+    original_sequence: list[Node], distance_matrix: DistanceMatrix
+) -> list[DeltaIntraNodes]:
     changes = set(combinations(original_sequence, 2))
     neighbourhood = []
     for change in changes:
         from_node, to_node = change
-        new_sequence = original_sequence.copy()
+        neighbourhood.append(DeltaIntraNodes(from_node, to_node, distance_matrix))
 
-        from_ind, to_ind = original_sequence.index(from_node), original_sequence.index(to_node)
-        new_sequence[from_ind], new_sequence[to_ind] = to_node, from_node
-
-        neighbourhood.append(new_sequence)
-
+    # Size: n*(n - 1)/2
     return neighbourhood
